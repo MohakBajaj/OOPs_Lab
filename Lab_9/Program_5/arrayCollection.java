dd5512396618b51247b9065b58c81bf83e0a5d42
Mohak Bajaj <bmohak87@gmail.com>
2022-11-13 23:01:54 +0530

    Patched ArrayListCode 📈

--- Lab_9/Program_5/arrayCollection.java
+++ Lab_9/Program_5/arrayCollection.java
@@ -6,35 +6,50 @@
 package Lab_9.Program_5;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class arrayCollection {
-    int[] arr = new int[10];
+    ArrayList<Integer> list = new ArrayList<Integer>();
 
-    public void take_input() {
+    public void takeInput() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 10 numbers: ");
-        for (int i = 0; i < 10; i++) {
-            arr[i] = sc.nextInt();
+        System.out.println("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
         }
         sc.close();
-    }
-
-    public int[] reverse(int arr[]) {
-        for (int i = 0; i < 10; i++) {
-            arr[i] = 10 - i;
+        System.out.println("The elements of the array are: ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
         }
-        return arr;
+        System.out.println();
     }
 
-    public void print(int arr[]) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(arr[i]);
+    public void duplicate() {
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        list2 = (ArrayList<Integer>) list.clone();
+        System.out.println("The duplicate array is: ");
+        for (int i = 0; i < list2.size(); i++) {
+            System.out.print(list2.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public void reverse() {
+        Collections.reverse(list);
+        System.out.println("The reversed array is: ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
         }
     }
 
     public static void main(String[] args) {
-        arrayCollection a = new arrayCollection();
-        a.take_input();
-        a.print(a.reverse(a.arr));
+        arrayCollection obj = new arrayCollection();
+        obj.takeInput();
+        obj.duplicate();
+        obj.reverse();
     }
 }
